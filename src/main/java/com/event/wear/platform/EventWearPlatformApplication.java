5b20f9c8b2ae6c9cfb1405b5567dc9d2334675af
Adriano Sebastian Cruz Palomino <u202210697@upc.edu.pe>
2024-05-25 13:47:14 -0500

    chore: initial commit

---
+++ src/main/java/com/event/wear/platform/EventWearPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.event.wear.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventWearPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventWearPlatformApplication.class, args);
+	}
+
+}
