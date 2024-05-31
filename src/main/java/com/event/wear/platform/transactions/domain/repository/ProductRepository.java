b5728239f5682296a4ff057efbf605f46d593950
christopherMore1 <155859499+christopherMore1@users.noreply.github.com>
2024-05-31 09:14:30 -0500

    Delete src/main/java/com/event/wear/platform/transactions/domain/repository directory
    Fix: Useless directory deleted

--- src/main/java/com/event/wear/platform/transactions/domain/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.event.wear.platform.transactions.domain.repository;
-
-import com.event.wear.platform.transactions.domain.model.entities.ProductItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductItem, Long> {
-}
