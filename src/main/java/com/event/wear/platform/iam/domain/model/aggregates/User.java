da397ba88e9fdcaac49c572fc3dda786915b53a3
Alexander Jair Castillo <142842509+U202211390@users.noreply.github.com>
2024-06-20 20:43:11 -0500

    Add: added BC User whit iam

---
+++ src/main/java/com/event/wear/platform/iam/domain/model/aggregates/User.java
@@ -0,0 +1,2 @@
+package com.event.wear.platform.iam.domain.model.aggregates;public class User {
+}
---
+++ src/main/java/com/event/wear/platform/iam/domain/model/valueobjects/Roles.java
@@ -0,0 +1,2 @@
+package com.event.wear.platform.iam.domain.model.valueobjects;public enum Roles {
+}
